e89d0ffb96032b6c007be85b5034dc9b58884d67
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-08-25 15:16:40 +0700

    v1.0.0 finished

--- src/pages/History.jsx
+++ src/pages/History.jsx
@@ -1,11 +1,14 @@
 import React from 'react'
 import GamesHistory from '../components/organism/GamesHistory'
+import BottomNavbar from '../components/organism/Navbar'
 
 function History() {
   return (
     <>
    
 <GamesHistory/>
+<BottomNavbar />
+
     </>
   )
 }
