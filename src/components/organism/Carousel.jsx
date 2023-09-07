28ee625b91aef7e753b8978ff96546a103e3ca56
Macbook <macbook@Macbooks-MacBook-Pro.local>
2023-09-07 11:45:23 +0700

    skeletons

--- src/components/organism/Carousel.jsx
+++ src/components/organism/Carousel.jsx
@@ -92,10 +92,11 @@ function Carousel() {
           )
         })
        
-      : <Box padding='6' boxShadow='lg' bg='white'>
-      <SkeletonCircle size='10' />
-      <SkeletonText mt='4' noOfLines={4} spacing='4' skeletonHeight='2' />
-    </Box>
+      : <Skeleton>
+  
+      <div>contents wrapped</div>
+      <div>won't be visible</div>
+    </Skeleton>
       }
    
    
